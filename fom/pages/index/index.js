be042209acafe431b99cb6d23d8af30a4d07e0cb
forevermistery <1258234227@qq.com>
2018-07-02 15:19:15 +0800

    miniProgram store

---
+++ fom/pages/index/index.js
@@ -0,0 +1,17 @@
+Page({
+    data: {
+        background: [
+            '../../image/swiper1.jpg',
+            '../../image/swiper1.jpg',
+            '../../image/swiper1.jpg'
+        ],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: true,
+        interval: 2000,
+        duration: 500
+    },
+    
+
+    
+})
